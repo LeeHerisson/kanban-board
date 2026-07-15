e48a3d2a4aacb83ca9a1228da5b24f84eae106bf
Lee Herisson <shaiakhmetovamir2006@gmail.com>
2026-07-15 12:25:21 +0600

    refactor(formatDate): conditionally hide year if it is current

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,14 @@
-export const formatDate = (date: string): string =>
-  new Intl.DateTimeFormat("en-GB", {
+export const formatDate = (date: string): string => {
+  const taskDate = new Date(date);
+  const currentYear = new Date().getFullYear();
+  const taskYear = taskDate.getUTCFullYear();
+
+  const options: Intl.DateTimeFormatOptions = {
     day: "numeric",
     month: "short",
-    year: "numeric",
     timeZone: "UTC",
-  }).format(new Date(date));
+    year: taskYear !== currentYear ? "numeric" : undefined,
+  };
+
+  return new Intl.DateTimeFormat("en-GB", options).format(taskDate);
+};
